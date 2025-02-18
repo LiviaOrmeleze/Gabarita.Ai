4b891990fb3e134c779e7fcebd93cc6e434553d5
Gabriela <gabrielasiqueiracaputti@gmail.com>
2025-02-18 09:17:50 -0300

    Adicionando atributos dentro da Model Planos

--- Gabarita.Ai/Gabarita.Ai/Models/Planos.cs
+++ Gabarita.Ai/Gabarita.Ai/Models/Planos.cs
@@ -2,5 +2,11 @@
 {
     public class Planos
     {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public string Preco { get; set; }
+        public string Duracao { get; set; }
+        public string Conteudo { get; set; }
     }
 }
