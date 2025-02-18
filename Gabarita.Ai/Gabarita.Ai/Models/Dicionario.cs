69a6a2ec3d31a298a8aaa09c909d657446118203
Aninha <aninha.clara030807@gmail.com>
2025-02-18 08:24:45 -0300

    Criado a Model Dicionario e um Enum Categoria Dicionario

---
+++ Gabarita.Ai/Gabarita.Ai/Models/Dicionario.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gabarita.Ai.Models
+{
+    public class Dicionario
+    {
+        public int Id { get; set; }
+        public string Termo { get; set; }
+        public string Definicao { get; set; }
+        public CategoriaDicionario Categoria { get; set; } // Usando o enum
+        public string? ExemploUso { get; set; }
+        public DateTime CriadoEm { get; set; } = DateTime.UtcNow;
+        public DateTime AtualizadoEm { get; set; } = DateTime.UtcNow;
+    }
+}
