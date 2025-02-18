6fc7a1a79ddb57d95a7335c0c7b0bfda6ed6eee5
Gabriela <gabrielasiqueiracaputti@gmail.com>
2025-02-18 09:13:02 -0300

    Criando Model de ConteudoEscolhido e adicionando atributos dentro

---
+++ Gabarita.Ai/Gabarita.Ai/Models/ConteudoEscolhido.cs
@@ -0,0 +1,12 @@
+﻿namespace Gabarita.Ai.Models
+{
+    public class ConteudoEscolhido
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Questoes { get; set; }
+        public string Videos { get; set; }
+        public string Textos { get; set; }
+        public string FlashCard { get; set; }
+    }
+}
