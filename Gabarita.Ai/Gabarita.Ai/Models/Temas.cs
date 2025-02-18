1911572df0b19e6f64aff38ed444e7fe9ba76b26
Livia Ormeleze <liviasenai.ormeleze@gmail.com>
2025-02-18 17:03:48 -0300

    Ajustando model Conteudo e tema

--- Gabarita.Ai/Gabarita.Ai/Models/Temas.cs
+++ Gabarita.Ai/Gabarita.Ai/Models/Temas.cs
@@ -5,5 +5,8 @@
         public Guid TemasId { get; set; }
         public string Niveis { get; set; }
         public string Conteudos { get; set; }
+
+        public Guid MateriasId { get; set; }
+        public Materias? Materias { get; set; }
     }
 }
