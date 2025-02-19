3e63935cf5c5006382a3fb9ea3548cf8b3c2788a
Livia Ormeleze <liviasenai.ormeleze@gmail.com>
2025-02-19 09:04:37 -0300

    Craindo os controllers

--- Gabarita.Ai/Gabarita.Ai/Data/GabaritaContext.cs
+++ Gabarita.Ai/Gabarita.Ai/Data/GabaritaContext.cs
@@ -14,6 +14,14 @@ namespace Gabarita.Ai.Data
         {
             base.OnModelCreating(builder);
         }
+        public DbSet<Gabarita.Ai.Models.ConteudoEscolhido> ConteudoEscolhido { get; set; } = default!;
+        public DbSet<Gabarita.Ai.Models.Cronograma> Cronograma { get; set; } = default!;
+        public DbSet<Gabarita.Ai.Models.Desempenho> Desempenho { get; set; } = default!;
+        public DbSet<Gabarita.Ai.Models.Dicionario> Dicionario { get; set; } = default!;
+        public DbSet<Gabarita.Ai.Models.FlashCard> FlashCard { get; set; } = default!;
+        public DbSet<Gabarita.Ai.Models.Livro> Livro { get; set; } = default!;
+        public DbSet<Gabarita.Ai.Models.Materias> Materias { get; set; } = default!;
+        public DbSet<Gabarita.Ai.Models.Orientacoes> Orientacoes { get; set; } = default!;
 
     }
 }
