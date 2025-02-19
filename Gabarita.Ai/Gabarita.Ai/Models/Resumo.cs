5c7f6aab6ea8eca8806d3511677f192bae9f3129
Gabriela <gabrielasiqueiracaputti@gmail.com>
2025-02-19 07:42:21 -0300

    Adicionando Model e classes de Resumo

---
+++ Gabarita.Ai/Gabarita.Ai/Models/Resumo.cs
@@ -0,0 +1,10 @@
+﻿namespace Gabarita.Ai.Models
+{
+    public class Resumo
+    {
+        public Guid ResumoId { get; set; }
+        public string Materia { get; set; }
+        public string Titulo { get; set; }
+        public string Texto { get; set; }
+    }
+}
