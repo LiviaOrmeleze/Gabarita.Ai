e8381aa62632de708a49afb7b3024a432605d889
Aninha <aninha.clara030807@gmail.com>
2025-02-19 07:43:33 -0300

    Criado a model FlashCard

---
+++ Gabarita.Ai/Gabarita.Ai/Models/FlashCard.cs
@@ -0,0 +1,10 @@
+﻿namespace Gabarita.Ai.Models
+{
+    public class FlashCard
+    {
+        public int FlashCardId { get; set; }
+        public string Materia { get; set; }
+        public string Pergunta { get; set; }
+        public string Resposta { get; set; }
+    }
+}
